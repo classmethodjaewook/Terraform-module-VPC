819ad1ebf8d17eda6b1a102c085a0b06da646055
classmethodjaewook <kim.jaewook@classmethod.jp>
2022-06-14 13:39:57 +0900

    push VPC

---
+++ module/vpc/vpc.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_vpc
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.cidr_public1
+  availability_zone = var.az1
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.cidr_public2
+  availability_zone = var.az2
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.cidr_private1
+  availability_zone = var.az1
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.cidr_private2
+  availability_zone = var.az2
+}
