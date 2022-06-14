b8ab08ce56cf4455ba65f6dc8eaa62153c62a428
classmethodjaewook <kim.jaewook@classmethod.jp>
2022-06-14 13:41:28 +0900

    push main VPC

---
+++ main_vpc.tf
@@ -0,0 +1,10 @@
+module "network" {
+  source   = "./module/vpc"
+  cidr_vpc = "10.0.0.0/16"
+  cidr_public1 = "10.0.1.0/24"
+  cidr_public2 = "10.0.2.0/24"
+  cidr_private1 = "10.0.11.0/24"
+  cidr_private2 = "10.0.12.0/24"
+  az1 = "ap-northeast-1a"
+  az2 = "ap-northeast-1c"
+}
